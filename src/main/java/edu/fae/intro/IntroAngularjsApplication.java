55f0aa670380ec3e7b59829715f55efb6b0ab0cd
Robson Luz <robson.luz@gmail.com>
2016-08-09 20:05:23 -0300

    Projeto inicial

---
+++ src/main/java/edu/fae/intro/IntroAngularjsApplication.java
@@ -0,0 +1,12 @@
+package edu.fae.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroAngularjsApplication.class, args);
+	}
+}
